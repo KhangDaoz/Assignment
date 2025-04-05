e0d1148affc051a04be69cf5d4e771f97aee8b5a
KhangDaoz <khangda01808@gmail.com>
2025-04-05 22:48:25 +0700

    05/04/2025

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print('Khang')
+import cloudscraper as cs
+from bs4 import BeautifulSoup as BS
+
+url = 'https://fbref.com/en/squads/18bb7c10/Arsenal-Stats'
+scaper = cs.create_scraper()
+response = scaper.get(url)
+# print(response.status_code)
+soup = BS(response.content, 'lxml')
+
+content = soup.find('div', id="content")
+
+all_stats_standard = content.find('div', id = 'all_stats_standard')
+table_container = all_stats_standard.table
+tbody = table_container.tbody
+rows = tbody.find_all('tr')
+
+for row in rows:
+    print(row.th.text)
+# print(table_container)
---
+++ tmp.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# date: 2019.09.21
+
+
+from bs4 import BeautifulSoup as BS
+import requests 
+import cloudscraper
+
+url = 'https://fbref.com/en/matches/033092ef/Northampton-Town-Lincoln-City-August-4-2018-League-Two'
+
+scaper = cloudscraper.create_scraper()
+response = scaper.get(url)
+print(response.status_code)
+
+soup = BS(response.content, 'html.parser')
+
+print(soup.prettify())
+
+# stats = soup.find('div', id="team_stats")
+
+# print(stats.prettify())
+
+# data = []
+# for row in stats.find_all('td'):
+#     text = row.get_text(strip=True)
+#     data.append(text)
+    
+# print('Possession:', data[0], '|', data[1])
+
+# text1, percent1 = data[2].split('—')
+# percent2, text2 = data[3].split('—')
+# text1 = text1.strip('\xa0')
+# text2 = text2.strip('\xa0')
+# print('Shots on Target:', text1, '|', percent1, '|', text2, '|', percent2)
+
+# text1, percent1 = data[4].split('—')
+# percent2, text2 = data[5].split('—')
+# text1 = text1.strip('\xa0')
+# text2 = text2.strip('\xa0')
+# print('Saves:', text1, '|', percent1, '|', text2, '|', percent2)
+
+    

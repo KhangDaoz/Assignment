051151f7a64fd7b4fe46b9e82922e1b508da1d7f
KhangDaoz <khangda01808@gmail.com>
2025-04-12 22:54:30 +0700

    12/04/2025 (Done Ex3)

--- Problem_II.py
+++ Problem_II.py
@@ -1,4 +1,5 @@
 import pandas 
+import matplotlib.pyplot as plt
 
 # Identify the top 3 players with the highest and lowest scores for each statistic.
 def identify_the_top_3_each_statistic(df):
@@ -21,40 +22,97 @@ def find_median_mean_and_standard_each_statistic(df):
     # get headers
     headers = df.columns[4:]
 
-    # calculate the median, mean and std all 
+    # calculate the median, mean and std all players of league
     all_team = df[headers].agg(['median', 'mean', 'std']).T
 
     # transpose dataframe to one row
-    new_columns = []    
-    values = []
+    new_columns = ['Team']    
+    values = ['All']
     for col in all_team.index:
         for stat in ['median', 'mean', 'std']:
             new_columns.append(f'{stat} of {col}')
             values.append(all_team.loc[col, stat])
     all_team = pandas.DataFrame([values], columns=new_columns)
-    all_team.index = ['All']
 
     # calculate the median, mean and std for each team
     mapped_headers = {}
     for header in headers:
         mapped_headers[header] = ['median', 'mean', 'std']
-    cal_df = df.groupby('Team').agg(mapped_headers)
+    cal_df = df.groupby('Team').agg(mapped_headers).reset_index()
 
     # rename the columns
     headers = cal_df.columns
     new_headers = []
     for x in headers:
-        new_headers.append(x[1] + ' of ' + x[0])
+        if x[0] == 'Team':
+            new_headers.append(x[0])
+        else:
+            new_headers.append(x[1] + ' of ' + x[0])
     cal_df.columns = new_headers
 
     # merge the all_team dataframe with the cal_df dataframe
     cal_df = pandas.concat([all_team, cal_df], axis = 0)
 
     # round float values to 2 decimal places
-    cal_df = cal_df.astype(float).round(2)
+    num_cols = cal_df.select_dtypes(include=['number']).columns
+    cal_df[num_cols] = cal_df[num_cols].astype(float).round(2)
+
+    # fill N/a values
+    cal_df = cal_df.replace('', 'N/a').fillna('N/a').reset_index(drop = True)
 
     # write to csv file
     cal_df.to_csv('result2.csv', encoding = 'utf-8-sig')
+
+def plot_histogram(df):
+    # all players of league
+    headers = df.columns[4:]    
+    for header in headers:
+        df_copy = df[header].dropna()
+        plt.hist(df_copy, bins = 30, color = 'red', edgecolor = 'black')
+        plt.title(f'Histogram of {header} Distribution')
+        plt.xlabel('Value of ' + header)
+        plt.ylabel('Frequency Players')
+        plt.grid(axis = 'y', color = 'black', linestyle = '--', linewidth = 0.5)
+        plt.show()  
+
+    # players of each team
+    for team, dfteam in df.groupby('Team'):
+        for header in headers:
+            df_copy = dfteam[header].dropna()
+            plt.hist(df_copy, bins = 30, color = 'red', edgecolor = 'black')
+            plt.title(f'Histogram of {header} Distribution for {team}')
+            plt.xlabel('Value of ' + header)
+            plt.ylabel('Frequency Players')
+            plt.grid(axis = 'y', color = 'black', linestyle = '--', linewidth = 0.5)
+            plt.show()
+
+def identify_team_with_highest_score(df):
+    # get the teams and headers
+    teams = sorted(df['Team'].unique())
+    headers = df.columns[4:]
+
+    # read the csv file
+    data = pandas.read_csv('result2.csv', index_col = False)
+    data = data.drop(index = 0).reset_index(drop = True)
+
+    # process the data to new dataframe
+    new_df = pandas.DataFrame(teams, columns = ['Team'])
+    for header in headers:
+        new_df[header] = (data['mean of ' + header])
+    
+    # identify the team with the highest scores for each statistic.
+    dict = {}
+    for team in teams:
+        dict[team] = 0
+    for header in headers:
+        idx = new_df[header].idxmax()
+        dict[new_df['Team'][idx]] += 1
+        print(f'{new_df["Team"][idx]} has the highest {header} score.')
+    
+    # find the team best team performing in the league
+    best_team = max(dict, key = dict.get)
+    print('-' * 70)
+    print(f'-> {best_team} is performing the best in the 2024-2025 Premier League season.')
 
 if __name__ == '__main__':
     # Read the CSV file into a DataFrame
@@ -66,4 +124,5 @@ if __name__ == '__main__':
 
     identify_the_top_3_each_statistic(df)
     find_median_mean_and_standard_each_statistic(df)
-
+    plot_histogram(df)
+    identify_team_with_highest_score(df)

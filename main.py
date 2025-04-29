9e70ef1ca04399e8a6cce642ccff9f85eca19002
KhangDaoz <khangda01808@gmail.com>
2025-04-29 08:49:51 +0700

    29/04/2025

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-url = 'https://www.footballtransfers.com/en/transfers/actions/confirmed/overview'
+def process_transfer_values(value):
+    value = value[1::]
+    n = len(value)
+    value = value[:n - 1:]
+    return float(value)
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
-    "Referer": "https://www.footballtransfers.com/en/transfers/confirmed/2024-2025/uk-premier-league",
-    "X-Requested-With": "XMLHttpRequest",
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-}
+# read data
+df = pd.read_csv('result.csv')
+df_transfer = pd.read_csv('player_transfer_values.csv')
 
-payload = {
-    "orderBy": "date_transfer",
-    "orderByDescending": 1,
-    "page": 1,
-    "pages": 0,
-    "pageItems": 25,
-    "countryId": all,
-    "season": 5845,
-    "tournamentId": 31,
-    "clubFromId": all,
-    "clubToId": all,
-}
+# preprocess to merge
+df = df[df['Minutes'] > 900]
+df['Age'] = df['Age'].apply(lambda x : float(f'{(int(x.split('-')[0]) + (int(x.split('-')[1]) / 365)):.2f}'))
+df_transfer['Player'] = df_transfer['Player'].replace('Igor Júlio', 'Igor')
+df_transfer['Transfer Value'] = df_transfer['Transfer Value'].apply(process_transfer_values)
 
-df = pd.DataFrame()
 
-for page in range(1, 15):
-    payload['page'] = page
-    response = requests.post(url, headers = headers, data = payload)
-    data = response.json()
-    cur_df = pd.DataFrame(data['records'])
-    df = pd.concat([df, cur_df], axis = 0, ignore_index = True)
+# merge dataframe
+df = pd.merge(df, df_transfer, how = 'left', on = ['Player']).dropna().reset_index(drop = True)
+df = df.drop(columns = ['Player', 'Nation', 'Team_x', 'Team_y'])
 
-df_transfer = df[['player_name', 'country_name', 'age', 'club_from_name', 'club_to_name', 'amount', 'date_transfer']]
-df_transfer = df_transfer.rename(columns = {'player_name' : 'Player'})
+print(df.shape)
 
-df = pd.read_csv('result.csv', encoding = 'utf-8-sig')
-df_transfer = df_transfer.merge(df[['Player', 'Minutes']], how = 'left', on = 'Player')
-df_transfer = df_transfer[df_transfer['Minutes'] > 900].reset_index(drop = True)
-print(df_transfer)
-# df_transfer.to_csv('test.csv', encoding = 'utf-8-sig')
+obj = (df.dtypes == 'object')
+object_cols = list(obj[obj].index)
+print("Categorical variables:",len(object_cols))
+print(object_cols)
+int_ = (df.dtypes == 'int')
+num_cols = list(int_[int_].index)
+print("Integer variables:",len(num_cols))
+
+fl = (df.dtypes == 'float')
+fl_cols = list(fl[fl].index)
+print("Float variables:",len(fl_cols))
+
+# df.to_csv('test.csv', encoding = 'utf-8-sig', index = False)
